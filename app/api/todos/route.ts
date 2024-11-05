f6bf1da8e53f90708d86419b471e11cf4955e25b
Joseph Watzson <49710538+joe-watson-sbf@users.noreply.github.com>
2024-11-04 21:22:19 -0500

    new

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -4,9 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
   const status = req.nextUrl.searchParams.get('status') === "completed" ? true : false;
   const search = req.nextUrl.searchParams.get('search'); 
-  
   const allTodos = await findAllTodos();
-
   const todos = search ? allTodos.filter(todo => todo.title.toLowerCase().includes((search.toLowerCase().trim()))) : allTodos;
   if (status) {
     return NextResponse.json(todos.filter(todo => todo.completed));
@@ -31,6 +29,7 @@ export async function POST(req: NextRequest) {
 export async function PUT(req: NextRequest) {
   try {
     const { id, completed } = await req.json();
+
     await updateTodo(id, completed);
     return NextResponse.json({ message: "Todo updated!" });
   } catch (error: any) {
@@ -42,6 +41,7 @@ export async function PUT(req: NextRequest) {
 
 export async function DELETE(req: NextRequest) {
   const {id} = await req.json();
+
   if (!id) {
     return NextResponse.json({ error: "Todo ID is required" }, { status: 400 });
   }
